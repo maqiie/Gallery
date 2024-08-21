2318aa0e7afcbe449e3dc84329bc2d3488ec0fab
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-21 21:22:22 +0300

    loaders

--- src/components/ScreenSaver.js
+++ src/components/ScreenSaver.js
@@ -230,77 +230,78 @@ const ScreenSaver = ({ projectId }) => {
         <FontAwesomeIcon icon={faArrowRight} size="lg" />
       </button>
       <style jsx>{`
-  .newtons-cradle {
-    --uib-size: 70px; /* Increased size */
-    --uib-speed: 1.2s;
-    --uib-color: #ffffff; /* Changed color to white */
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: var(--uib-size);
-    height: var(--uib-size);
-  }
+.newtons-cradle {
+ --uib-size: 50px;
+ --uib-speed: 1.2s;
+ --uib-color: #ffffff;
+ position: relative;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ width: var(--uib-size);
+ height: var(--uib-size);
+}
 
-  .newtons-cradle__dot {
-    position: relative;
-    display: flex;
-    align-items: center;
-    height: 100%;
-    width: 30%; /* Slightly increased width */
-    transform-origin: center top;
-  }
+.newtons-cradle__dot {
+ position: relative;
+ display: flex;
+ align-items: center;
+ height: 100%;
+ width: 25%;
+ transform-origin: center top;
+}
 
-  .newtons-cradle__dot::after {
-    content: "";
-    display: block;
-    width: 100%;
-    height: 30%; /* Increased size */
-    border-radius: 50%;
-    background-color: var(--uib-color);
-  }
+.newtons-cradle__dot::after {
+ content: '';
+ display: block;
+ width: 100%;
+ height: 25%;
+ border-radius: 50%;
+ background-color: var(--uib-color);
+}
 
-  .newtons-cradle__dot:first-child {
-    animation: swing var(--uib-speed) linear infinite;
-  }
+.newtons-cradle__dot:first-child {
+ animation: swing var(--uib-speed) linear infinite;
+}
 
-  .newtons-cradle__dot:last-child {
-    animation: swing2 var(--uib-speed) linear infinite;
-  }
+.newtons-cradle__dot:last-child {
+ animation: swing2 var(--uib-speed) linear infinite;
+}
 
-  @keyframes swing {
-    0% {
-      transform: rotate(0deg);
-      animation-timing-function: ease-out;
-    }
+@keyframes swing {
+ 0% {
+  transform: rotate(0deg);
+  animation-timing-function: ease-out;
+ }
 
-    25% {
-      transform: rotate(70deg);
-      animation-timing-function: ease-in;
-    }
+ 25% {
+  transform: rotate(70deg);
+  animation-timing-function: ease-in;
+ }
 
-    50% {
-      transform: rotate(0deg);
-      animation-timing-function: linear;
-    }
-  }
+ 50% {
+  transform: rotate(0deg);
+  animation-timing-function: linear;
+ }
+}
 
-  @keyframes swing2 {
-    0% {
-      transform: rotate(0deg);
-      animation-timing-function: linear;
-    }
+@keyframes swing2 {
+ 0% {
+  transform: rotate(0deg);
+  animation-timing-function: linear;
+ }
 
-    50% {
-      transform: rotate(0deg);
-      animation-timing-function: ease-out;
-    }
+ 50% {
+  transform: rotate(0deg);
+  animation-timing-function: ease-out;
+ }
 
-    75% {
-      transform: rotate(-70deg);
-      animation-timing-function: ease-in;
-    }
-  }
+ 75% {
+  transform: rotate(-70deg);
+  animation-timing-function: ease-in;
+ }
+}
+
 `}</style>
 
     </div>
