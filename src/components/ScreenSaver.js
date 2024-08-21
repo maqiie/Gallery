5ee814dbda100e365e927008ac1a9a8dd9668a5a
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-21 20:14:11 +0300

    loaders

--- src/components/ScreenSaver.js
+++ src/components/ScreenSaver.js
@@ -441,7 +441,7 @@ const ScreenSaver = ({ projectId }) => {
   return (
     <div
       ref={screenSaverRef}
-      className="relative w-full h-full overflow-hidden"
+      className="relative w-screen h-screen overflow-hidden bg-black"
     >
       {transitions((style, item) => (
         <animated.div
@@ -457,31 +457,33 @@ const ScreenSaver = ({ projectId }) => {
           }}
         >
           {loading && (
-            <section className="loader">
-              <div>
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="loader">
                 <div>
-                  <span className="one h6"></span>
-                  <span className="two h3"></span>
+                  <div>
+                    <span className="one"></span>
+                    <span className="two"></span>
+                  </div>
                 </div>
-              </div>
 
-              <div>
                 <div>
-                  <span className="one h1"></span>
+                  <div>
+                    <span className="one"></span>
+                  </div>
                 </div>
-              </div>
 
-              <div>
                 <div>
-                  <span className="two h2"></span>
+                  <div>
+                    <span className="two"></span>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <span className="one"></span>
+                  </div>
                 </div>
               </div>
-              <div>
-                <div>
-                  <span className="one h4"></span>
-                </div>
-              </div>
-            </section>
+            </div>
           )}
           {item && item.content_type && item.content_type.startsWith("video") ? (
             <video
@@ -525,15 +527,16 @@ const ScreenSaver = ({ projectId }) => {
           display: flex;
           justify-content: center;
           align-items: center;
-          position: relative;
-          width: 90px;
-          height: 103px;
+          width: 100%; /* Full width */
+          height: 100%; /* Full height */
+          position: absolute;
+          background: transparent; /* Make sure loader is visible on top */
         }
 
         .loader div {
           position: absolute;
-          width: 50px;
-          height: 31px;
+          width: 80px; /* Adjust size */
+          height: 80px; /* Adjust size */
         }
 
         .rot {
@@ -561,173 +564,17 @@ const ScreenSaver = ({ projectId }) => {
         .loader div div span {
           position: absolute;
           width: 4px;
-          height: 0%;
-          background: #053146;
+          height: 100%;
+          background: white; /* Loader bar color */
           z-index: 999999;
         }
 
-        .h1 {
-          left: 0;
-          animation: load1 3.2s ease infinite;
-        }
-
-        .h2 {
-          right: 0;
-          animation: load2 3.2s ease 0.4s infinite;
-        }
-
-        .h3 {
-          right: 0;
-          animation: load3 3.2s ease 0.8s infinite;
-        }
-
-        .h4 {
-          top: 10px;
-          left: 23px;
-          animation: load4 3.2s ease 1s infinite;
-          transform: rotate(90deg);
-        }
-
-        .h5 {
-          bottom: 0;
-          animation: load5 3.2s ease 1.2s infinite;
-        }
-
-        .h6 {
-          left: 0;
-          animation: load6 3.2s ease 1.3s infinite;
-        }
-
-        @keyframes load1 {
+        @keyframes spin {
           0% {
-            bottom: 0;
-            height: 0;
+            transform: rotate(0deg);
           }
-
-          6.944444444% {
-            bottom: 0;
-            height: 100%;
-          }
-
-          50% {
-            top: 0;
-            height: 100%;
-          }
-
-          59.944444433% {
-            top: 0;
-            height: 0;
-          }
-        }
-
-        @keyframes load2 {
-          0% {
-            top: 0;
-            height: 0;
-          }
-
-          6.944444444% {
-            top: 0;
-            height: 100%;
-          }
-
-          50% {
-            bottom: 0;
-            height: 100%;
-          }
-
-          59.944444433% {
-            bottom: 0;
-            height: 0;
-          }
-        }
-
-        @keyframes load3 {
-          0% {
-            top: 0;
-            height: 0;
-          }
-
-          6.944444444% {
-            top: 0;
-            height: 100%;
-          }
-
-          50% {
-            bottom: 0;
-            height: 100%;
-          }
-
-          59.94444443% {
-            bottom: 0;
-            height: 0;
-          }
-        }
-
-        @keyframes load4 {
-          0% {
-            top: 37px;
-            left: 23px;
-            height: 134%;
-          }
-
-          6.944444444% {
-            top: 10px;
-            height: 134%;
-          }
-
-          50% {
-            bottom: 10px;
-            height: 134%;
-          }
-
-          59.94444443% {
-            bottom: 0;
-            height: 0;
-          }
-        }
-
-        @keyframes load5 {
-          0% {
-            bottom: 0;
-            height: 0;
-          }
-
-          6.944444444% {
-            bottom: 0;
-            height: 100%;
-          }
-
-          50% {
-            top: 0;
-            height: 100%;
-          }
-
-          59.94444443% {
-            top: 0;
-            height: 0;
-          }
-        }
-
-        @keyframes load6 {
-          0% {
-            bottom: 0;
-            height: 0;
-          }
-
-          6.944444444% {
-            bottom: 0;
-            height: 100%;
-          }
-
-          50% {
-            top: 0;
-            height: 100%;
-          }
-
-          59.94444443% {
-            top: 0;
-            height: 0;
+          100% {
+            transform: rotate(360deg);
           }
         }
       `}</style>
